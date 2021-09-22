1d5745d4ed843d622581069551710ff3c7d9b29a
abreaux26 <angelbreaux26@gmail.com>
2021-09-22 10:29:56 -0500

    Update to render json

--- app/controllers/api/v1/overlaps_controller.rb
+++ app/controllers/api/v1/overlaps_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::OverlapsController < ApplicationController
   def index
-    render plain: 'YOU CAN ACCESS THIS API'
+    # render plain: 'YOU CAN ACCESS THIS API'
+    render json: {
+      data: [
+        {
+          overlap_error_message: "YOU CAN ACCESS THIS API"
+        }
+      ]
+    }
   end
 end
